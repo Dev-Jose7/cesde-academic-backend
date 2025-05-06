b29bed9d4ef2aa9438bffb09a67e9689c9b6c52d
Jose Navarro <jfnr398@gmail.com>
2025-05-05 22:10:50 -0500

    fix: se modifica interfaz del repositorio de la entidad ClaseHorario

--- src/main/java/org/cesde/academic/repository/ClaseHorarioRepository.java
+++ src/main/java/org/cesde/academic/repository/ClaseHorarioRepository.java
@@ -1,13 +1,14 @@
 package org.cesde.academic.repository;
 
 import org.cesde.academic.model.ClaseHorario;
+import org.cesde.academic.model.ClaseHorarioId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ClaseHorarioRepository extends JpaRepository<ClaseHorario, Integer> {
+public interface ClaseHorarioRepository extends JpaRepository<ClaseHorario, ClaseHorarioId> {
     List<ClaseHorario> findByClase_Id(Integer claseId);
     List<ClaseHorario> findByHorario_Id(Integer horarioId);
 }
