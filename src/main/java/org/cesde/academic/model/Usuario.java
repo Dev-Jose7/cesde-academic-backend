795c293b95ab84e7c9c459ac28f05a82ec518acd
Jose Navarro <jfnr398@gmail.com>
2025-05-06 12:47:46 -0500

    feat: se agregan restricciones a clases modelo

--- src/main/java/org/cesde/academic/model/Usuario.java
+++ src/main/java/org/cesde/academic/model/Usuario.java
@@ -21,21 +21,20 @@ public class Usuario {
 
     @NotNull(message = "El nombre no puede ser nulo")
     @Size(min = 1, max = 255, message = "El nombre debe tener entre 1 y 255 caracteres")
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String nombre;
 
     @NotNull(message = "El correo no puede ser nulo")
     @Email(message = "El correo debe ser válido")
     @Size(max = 255, message = "El correo no puede exceder los 255 caracteres")
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, unique = true)
     private String correo;
 
     @NotNull(message = "La contraseña no puede ser nula")
     @Size(min = 6, max = 255, message = "La contraseña debe tener al menos 6 caracteres y máximo 255")
-    @Column(nullable = false, length = 255)
+    @Column(name = "contraseña", nullable = false)
     private String contrasena; // Renombrado de "contraseña" a "contrasena" (opcional)
 
-    @NotNull(message = "El tipo de usuario no puede ser nulo")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
     private Tipo tipo;
