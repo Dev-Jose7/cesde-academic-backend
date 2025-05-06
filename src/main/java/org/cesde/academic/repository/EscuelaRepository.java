9decfdd3a5fea07542a2ee9c4c726011d106d369
Jose Navarro <jfnr398@gmail.com>
2025-05-06 18:15:58 -0500

    feat: se definen contratos a las interfaces de los repositorios

--- src/main/java/org/cesde/academic/repository/EscuelaRepository.java
+++ src/main/java/org/cesde/academic/repository/EscuelaRepository.java
@@ -4,6 +4,9 @@ import org.cesde.academic.model.Escuela;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EscuelaRepository extends JpaRepository<Escuela, Integer> {
+    Optional<Escuela> findByNombre(String nombre);
 }
