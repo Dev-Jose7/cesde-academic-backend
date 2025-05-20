d85e4e91d2c5ecf80d2c9d8a8f0b658c2f37bf53
Jose Navarro <jfnr398@gmail.com>
2025-05-19 21:30:12 -0500

    chore: se cambia origen enfocado al entorno de desarrollo

--- src/main/java/org/cesde/academic/config/CorsConfig.java
+++ src/main/java/org/cesde/academic/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todos los endpoints
-                        .allowedOrigins("https://cesde-academic-app-production.up.railway.app")
+                        .allowedOrigins("https://cesde-academic-app-development.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
