f19772bfa173e8158bbf067ab1793610cdf19070
Jose Navarro <jfnr398@gmail.com>
2025-05-25 01:32:38 -0500

    feat: se crean dtos de petición y respuesta a las entidades Role y Permission

---
+++ src/main/java/org/cesde/academic/dto/response/PermissionResponseDTO.java
@@ -0,0 +1,13 @@
+package org.cesde.academic.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class PermissionResponseDTO {
+    private Integer id;
+    private String nombre;
+}
