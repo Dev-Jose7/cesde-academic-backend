0a79774e478da50b175906f059e2e4005f177df1
Jose Navarro <jfnr398@gmail.com>
2025-05-25 00:16:58 -0500

    feat: se crean entidades Role, Permission y se actualiza entidad Usuario para implementar Spring Security

---
+++ src/main/java/org/cesde/academic/model/Permission.java
@@ -0,0 +1,30 @@
+package org.cesde.academic.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
