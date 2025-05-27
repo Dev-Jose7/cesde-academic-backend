877bae2c0bd2d2e339f75adfd8e44c16a31aead0
Jose Navarro <jfnr398@gmail.com>
2025-05-27 01:47:11 -0500

    refactor: se inhabilitan autorizaciones indicadas en la capa controlador de la entidad Usuario

--- src/main/java/org/cesde/academic/controller/UsuarioController.java
+++ src/main/java/org/cesde/academic/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.cesde.academic.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +21,7 @@ public class UsuarioController {
     private IUsuarioService usuarioService;
 
     @PostMapping("/crear")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('CREATE')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('CREATE')")
     // La anotación @PreAuthorize permite proteger métodos según la autenticación o autorización del usuario actual, usando expresiones SpEL (Spring Expression Language). La puedes poner en métodos de controladores o servicios.
     public ResponseEntity<UsuarioResponseDTO> createUsuario(@Valid @RequestBody UsuarioRequestDTO request){
         UsuarioResponseDTO usuario = usuarioService.createUsuario(request);
@@ -29,7 +29,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/lista")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<List<UsuarioResponseDTO>> getListaUsuarios(){
         List<UsuarioResponseDTO> usuarios = usuarioService.getUsuarios();
         return usuarios.isEmpty()
@@ -38,14 +38,14 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<UsuarioResponseDTO> getUsuarioById(@PathVariable Integer id){
         UsuarioResponseDTO usuario = usuarioService.getUsuarioById(id);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
     @GetMapping("/buscar/nombre/{nombre}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<List<UsuarioResponseDTO>> getUsuarioByNombre(@PathVariable String nombre){
         List<UsuarioResponseDTO> usuarios = usuarioService.getUsuarioByNombre(nombre);
         return usuarios.isEmpty()
@@ -54,7 +54,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/buscar/cedula/{cedula}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<List<UsuarioResponseDTO>> getUsuarioByCedula(@PathVariable String cedula){
         List<UsuarioResponseDTO> usuarios = usuarioService.getUsuarioByCedula(cedula);
         return usuarios.isEmpty()
@@ -63,7 +63,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/buscar/correo/{correo}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<List<UsuarioResponseDTO>> getUsuarioByCorreo(@PathVariable String correo){
         List<UsuarioResponseDTO> usuarios = usuarioService.getUsuarioByCorreo(correo);
         return usuarios.isEmpty()
@@ -72,7 +72,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/buscar/tipo/{tipo}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('READ')")
     public ResponseEntity<List<UsuarioResponseDTO>> getUsarioByTipo(@PathVariable TipoUsuario tipo){
         List<UsuarioResponseDTO> usuarios = usuarioService.getUsuarioByTipo(tipo);
         return usuarios.isEmpty()
@@ -81,14 +81,14 @@ public class UsuarioController {
     }
 
     @PutMapping("/editar/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('UPDATE')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DEV') and hasAuthority('UPDATE')")
     public ResponseEntity<UsuarioResponseDTO> updateUsuario(@PathVariable Integer id, @Valid @RequestBody UsuarioRequestDTO request){
         UsuarioResponseDTO usuario = usuarioService.updateUsuario(id, request);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
     @DeleteMapping("/remover/{id}")
-    @PreAuthorize("hasRole('ROLE_DEV') and hasAuthority('DELETE')")
+//    @PreAuthorize("hasRole('ROLE_DEV') and hasAuthority('DELETE')")
     public ResponseEntity<Void> deleteUsuario(@PathVariable Integer id){
         usuarioService.deleteUsuario(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
