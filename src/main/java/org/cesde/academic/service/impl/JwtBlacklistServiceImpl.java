b44fc45b5056598a9eed3034d0c2507212445a25
Jose Navarro <jfnr398@gmail.com>
2025-05-30 17:56:03 -0500

    feat: se modifica capa servicio de la entidad JwtBlacklist para implementar refresh tokens

--- src/main/java/org/cesde/academic/service/impl/JwtBlacklistServiceImpl.java
+++ src/main/java/org/cesde/academic/service/impl/JwtBlacklistServiceImpl.java
@@ -1,39 +1,46 @@
 package org.cesde.academic.service.impl;
 
 import org.cesde.academic.model.JwtBlacklist;
+import org.cesde.academic.model.Usuario;
 import org.cesde.academic.repository.JwtBlacklistRepository;
 import org.cesde.academic.service.IJwtBlacklistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Service
 public class JwtBlacklistServiceImpl implements IJwtBlacklistService {
 
     @Autowired
-    private JwtBlacklistRepository jwtblacklistrepository;
+    private JwtBlacklistRepository jwtBlacklistRepository;
 
     @Override
-    public void createBlacklistToken(String token, Date expiracion) {
-        jwtblacklistrepository.save(createEntity(token, expiracion));
-    }
-
-    @Override
-    public boolean isTokenBlacklisted(String token) {
-        return jwtblacklistrepository.existsByToken(token);
-    }
-
-    @Override
-    public void deleteTokenBlacklist() {
-        jwtblacklistrepository.deleteExpiredTokens();
-    }
-
-    private JwtBlacklist createEntity(String token, Date expiracion){
+    public void blacklistTokens(String accessToken, Date accessExp, String refreshToken, Date refreshExp, Usuario usuario) {
         JwtBlacklist jwtBlacklist = new JwtBlacklist();
-        jwtBlacklist.setToken(token);
-        jwtBlacklist.setExpiracion(expiracion);
-        return jwtBlacklist;
+        jwtBlacklist.setAccessToken(accessToken);
+        jwtBlacklist.setAccessExpiracion(accessExp);
+        jwtBlacklist.setRefreshToken(refreshToken);
+        jwtBlacklist.setRefreshExpiracion(refreshExp);
+        jwtBlacklist.setUsuario(usuario);
+        jwtBlacklistRepository.save(jwtBlacklist);
+    }
+
+    @Override
+    public boolean isAccessTokenBlacklisted(String accessToken) {
+        return jwtBlacklistRepository.existsByAccessToken(accessToken);
+    }
+
+    @Override
+    public boolean isRefreshTokenBlacklisted(String refreshToken) {
+        return jwtBlacklistRepository.existsByRefreshToken(refreshToken);
+    }
+
+    @Override
+    @Transactional
+    public void deleteExpiredTokens() {
+        jwtBlacklistRepository.deleteExpiredAccessTokens();
+        jwtBlacklistRepository.deleteExpiredRefreshTokens();
     }
 }
