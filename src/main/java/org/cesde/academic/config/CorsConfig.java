7f27284e524a757e17e87f6ce0c2bc6072b41ade
Jose Navarro <jfnr398@gmail.com>
2025-06-04 15:37:50 -0500

    chore: se crea clase de configuración CORS

---
+++ src/main/java/org/cesde/academic/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package org.cesde.academic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Aplica a todas las rutas del backend
+                        .allowedOrigins(
+                                "https://cesde-academic.netlify.app",
+                                "https://cesde-academic-analytics-production.up.railway.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
