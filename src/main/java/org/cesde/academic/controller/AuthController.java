da0e0edf3405657b7c3204612e54c37612156b21
Jose Navarro <jfnr398@gmail.com>
2025-06-05 00:07:49 -0500

    refactor: se eliminan importaciones de clases no utilizadas

--- src/main/java/org/cesde/academic/controller/AuthController.java
+++ src/main/java/org/cesde/academic/controller/AuthController.java
@@ -1,36 +1,25 @@
 package org.cesde.academic.controller; // Define el paquete donde está ubicada esta clase
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.validation.Valid;
 import org.cesde.academic.dto.request.AuthRequestDTO;
 import org.cesde.academic.dto.request.RefreshTokenRequest;
 import org.cesde.academic.dto.response.AuthResponseDTO;
-import org.cesde.academic.dto.response.UsuarioResponseDTO;
 import org.cesde.academic.enums.TipoToken;
 import org.cesde.academic.enums.TipoUsuario;
-import org.cesde.academic.exception.RecursoNoEncontradoException;
 import org.cesde.academic.exception.TipoIncorrectoException;
-import org.cesde.academic.model.JwtBlacklist;
 import org.cesde.academic.model.Usuario;
-import org.cesde.academic.repository.UsuarioRepository;
 import org.cesde.academic.service.IJwtBlacklistService;
-import org.cesde.academic.service.IUsuarioService;
 import org.cesde.academic.service.impl.UserDetailsServiceImpl;
 import org.cesde.academic.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager; // Componente principal para manejar autenticaciones
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken; // Token de autenticación basado en nombre de usuario y contraseña
 import org.springframework.security.core.Authentication; // Representa una autenticación exitosa
-import org.springframework.security.core.AuthenticationException; // Excepción lanzada si la autenticación falla
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*; // Anotaciones para controladores REST
 
-import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController // Indica que esta clase es un controlador REST que devuelve datos (no vistas HTML)
@@ -106,7 +95,7 @@ public class AuthController {
         try {
             TipoUsuario.valueOf(tipo.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw  new TipoIncorrectoException("Tipo de usuario inválido");
+            throw new TipoIncorrectoException("Tipo de usuario inválido");
         }
 
         String token = request.substring(7);
